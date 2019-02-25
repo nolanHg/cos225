6006bf0481d2460b37ea79245fc5eac0fc2bcad3
Nolan Gagnon <nolan.gagnon@maine.edu>
2019-02-25 12:12:10 -0500

    removed a4

--- a4/EmptyCollectionException.java
+++
@@ -1,9 +0,0 @@
-@SuppressWarnings ("serial")
-public class EmptyCollectionException extends RuntimeException {
-	
-		public EmptyCollectionException ( )
-		{	
-			super ("The collection is empty.");	
-		}
-}
-
